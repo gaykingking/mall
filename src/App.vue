46b245b4458c6e788bcd7b76a750cb4ef46d07e7
lingsheng <1012616789@qq.com>
2020-08-12 22:44:17 +0800

    學習

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import storage from './storage'
+//import storage from './storage'
 export default {
   name: 'App',
   components: {
@@ -21,7 +21,19 @@ export default {
     //storage.setItem('user',{a:1});
     //storage.setItem('abc',{a:1},'user');
     //storage.clear('a');
-    storage.clear('a','user');
+    //storage.clear('a','user');
+    //本地加载请求静态json文件的形式
+    // this.axios.get('/mock/user/login.json').then((res)=>{
+    //   this.res=res;
+    // })
+    //通过easy-mock平台实现数据mock
+    //  this.axios.get('/mock/user/login.json').then((res)=>{
+    //    this.res=res;
+    //  })
+    //本地集成mockjs实现数据mock
+    this.axios.get('/user/login').then((res)=>{
+      this.res=res;
+    });
   }
 }
 </script>
