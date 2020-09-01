2fd0dcecee9999a08c7e55ad7327e7d34e3cd5a5
lingsheng <1012616789@qq.com>
2020-09-01 23:50:23 +0800

    header hover事件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        nav-footer
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="https://www.imooc.com/u/1343480" target="_blank">晟临天下主页</a><span>|</span>
+            <a href="">Vue全栈学习</a><span>|</span>
+            <a href="">React全家桶课程</a><span>|</span>
+            <a href="">微信支付专项课程</a><span>|</span>
+        </div>
+        <div class="copyright">Copyright ©2019 <span class="domain">mi.futurefe.com All Rights Reserved.</span></div>
     </div>
 </template>
 <script>
@@ -8,3 +18,37 @@ export default {
     name:'nav-footer'
 }
 </script>
+<style lang="scss" scoped>
+    .footer{
+        height:234px;
+        border-top: 4px solid #FF6600;
+        background-color: #333333;
+        color: #999999;
+        font-size: 16px;
+        text-align: center;
+        .footer-logo{
+            margin-top: 46px;
+            margin-bottom: 31px;
+            img{
+                width: 53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: #999999;
+                display: inline-block;
+            }
+            span{
+                margin: 0 10px;
+            }
+            margin-bottom: 13px;
+        }
+        .copyright{
+            .domain{
+                color: #FF6600;
+            }
+        }
+    }
+</style>
---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="service">
+        <div class="container">
+            sdfafd
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'service-bar'
+}
+</script>
+<style lang="scss">
+    
+</style>
